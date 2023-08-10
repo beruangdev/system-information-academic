3921aa63ac687b33f87ab687f84682f328d1dd96
beruangdev <beruangdev@gmail.com>
2023-08-11 06:24:18 +0700

    =build 1691709846579

--- concurrently/push.js
+++ concurrently/push.js
@@ -1,26 +1,9 @@
 import concurrently from "concurrently";
-import { resolve } from "path";
-import { exec, spawn } from "child_process";
-
-// exec("ls ./", (error, stdout, stderr) => {
-//   if (error) {
-//       console.log(`error: ${error.message}`);
-//       return;
-//   }
-//   if (stderr) {
-//       console.log(`stderr: ${stderr}`);
-//       return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-// });
-
-// const lsChildProcess = spawn("ls");
-// lsChildProcess.on("exit", () => console.log("the ls command finished"));
 
 const { result } = concurrently(
     [
         {
-            command: `git add . && git commit -m="build ${Date.now()}" && git push`,
+            command: `pnpm build && git add . && git commit -m="build ${Date.now()}" && git push`,
             name: "script",
             env: { APP_ENV: "production" },
         },
